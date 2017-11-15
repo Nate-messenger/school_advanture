5a12e5d45f81a8a63754bb604b61691dce33bfae
nate <nate@Nathan-Laptop>
2017-11-15 13:31:17 -0500

    imagehelper

---
+++ hhss-adventure-with-nate/src/hhss/adventure/with/nate/HhssAdventureWithNate.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhss.adventure.with.nate;
+
+/**
+ *
+ * @author nate
+ */
+public class HhssAdventureWithNate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
