77255c4011d7f1b02db5690bd5741764e9ca61c0
millc9988 <millc9988@HRH-IC0027327.wrdsb.ca>
2017-11-15 10:02:33 -0500

    Initial Master

---
+++ src/hhssadventure/HHSSAdventure.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hhssadventure;
+
+/**
+ *
+ * @author millc9988
+ */
+public class HHSSAdventure {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
