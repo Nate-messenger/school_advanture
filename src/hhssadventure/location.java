4355e80c65e3a81dda14d5ecf244656727a56943
millc9988 <millc9988@HRH-IC0037387.wrdsb.ca>
2017-11-16 11:16:38 -0500

    Msip work

--- src/hhssadventure/location.java
+++ src/hhssadventure/location.java
@@ -5,6 +5,9 @@
 package hhssadventure;
 
 import java.awt.image.BufferedImage;
+import java.io.FileReader;
+import java.net.URL;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +18,20 @@ public class location {
     private String east;
     private String south;
     private String west;
-    private location (){
-        
+    public location (FileReader file, String spot){
+        Scanner in = new Scanner(file);
+        int i = 0;
+        System.out.println(in.nextLine());
+        while(spot!=in.nextLine()){
+            System.out.println(in.nextLine());
+            in.nextLine();
+        }
+        in.nextLine();
+        in.next();
+        north = in.next(); 
+        east = in.next(); 
+        south = in.next(); 
+        west = in.next(); 
+        System.out.println(north+east+south+west);
     }
 }
